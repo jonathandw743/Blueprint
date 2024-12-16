a788179f6c22fcaac5b9124e7a48c94890041c4a
jonathandw743 <jonathandw414@gmail.com>
2024-12-16 00:47:03 +0000

    placeholder return values

--- assets/shaders/brainstorm.glsl
+++ assets/shaders/brainstorm.glsl
@@ -39,24 +39,23 @@ float parabola( float x, float k ){
 }
 
 vec4 greyscale(vec4 col) {
-
+    return vec4(0);
 }
 
 vec4 gaussian_blur(sampler2D jokers_sampler, ivec2 texture_coords) {
-
+    return vec4(0);
 }
 
 vec2 sobel_filter(sampler2D jokers_sampler, ivec2 texture_coords) {
-
+    return vec2(0);
 }
 
 vec4 canny_edges(sampler2D jokers_sampler, ivec2 texture_coords) {
-
+    return vec4(0);
 }
 
 
 // tex_coords is the tex coords in the big Jokers.png texture [0, 1]
-
 vec4 effect( vec4 colour, sampler2D jokers_sampler, vec2 texture_coords, vec2 screen_coords )
 {   
     // uint width, height;
@@ -64,7 +63,8 @@ vec4 effect( vec4 colour, sampler2D jokers_sampler, vec2 texture_coords, vec2 sc
     vec4 tex = texture(jokers_sampler, texture_coords);
 	// return vec4(tex, 0.0, 1.0);
     vec2 s = textureSize(jokers_sampler, 0);
-    return vec4(0.5 * s / vec2(710, 1520), 0.0, 1.0);
+    //return vec4(0.5 * s / vec2(710, 1520), 0.0, 1.0);
+    return vec4(1.0, 0.0, 0.0, 1.0);
 
 	//vec3 lightest = vec3(0.776, 0.824, 0.988);
 	//vec3 darkest = vec3(0.831, 0.376, 0.243);
